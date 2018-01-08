432a3099860c88ce9c5634965275e5340f47491d
Dustin Huang <mail@dustinhuang.com>
2018-01-08 15:29:42 -0800

    Add horizontal game logic

--- index.js
+++ index.js
@@ -14,16 +14,32 @@ class Game {
     ];
     this.player = 'X';
   }
+
+  display() {
+    this.board.forEach(row => console.log(row.join(' ')));
+  }
+
+  move(s) {
+    if (/[XO]/.test(this.board[Math.floor((s - 1) / 3)][(s - 1) % 3])) {
+      console.log('Invalid Move!');
+    } else {
+      this.board[Math.floor((s - 1) / 3)][(s - 1) % 3] = this.player;
+      this.player = this.player === 'X' ? 'O' : 'X';
+    }
+
+    return this.checkWinner();
+  }
+
+  checkWinner() {
+    return this.board.some((row) => row[0] === row[1] && row[1] === row[2]);
+  }
 }
 
 const prompt = function(game) {
   return new Promise((resolve, reject) => {
-    game.board.forEach(row => console.log(row.join(' ')));
-
+    game.display();
     rl.question(`${game.player} move: `, (s) => {
-      game.board[Math.floor((s - 1) / 3)][(s - 1) % 3] = game.player;
-      game.player = game.player === 'X' ? 'O' : 'X';
-      resolve();
+      resolve(s);
     });
   })
 };
@@ -32,7 +48,12 @@ const prompt = function(game) {
   const game = new Game();
   while(true) {
 
-   await prompt(game);
+    const s = await prompt(game);
+    if (game.move(s)) {
+      game.display();
+      console.log(`${game.player === 'X' ? 'O' : 'X'} won!`);
+      break;
+    }
   }
 
   rl.close();
