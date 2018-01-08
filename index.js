3aa81c72b9db6beca4ef19b4817408f2489d7393
Dustin Huang <mail@dustinhuang.com>
2018-01-08 15:12:51 -0800

    Add player moves

--- index.js
+++ index.js
@@ -5,20 +5,34 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const prompt = function() {
-  return new Promise((resolve, reject) => {
-    rl.question("X move: ", (a) => {
-      console.log(a);
+class Game {
+  constructor() {
+    this.board = [
+      ['1','2','3'],
+      ['4','5','6'],
+      ['7','8','9'],
+    ];
+    this.player = 'X';
+  }
+}
 
+const prompt = function(game) {
+  return new Promise((resolve, reject) => {
+    game.board.forEach(row => console.log(row.join(' ')));
+
+    rl.question(`${game.player} move: `, (s) => {
+      game.board[Math.floor((s - 1) / 3)][(s - 1) % 3] = game.player;
+      game.player = game.player === 'X' ? 'O' : 'X';
       resolve();
     });
   })
 };
 
 (async function gameLoop() {
+  const game = new Game();
   while(true) {
 
-   await prompt();
+   await prompt(game);
   }
 
   rl.close();
